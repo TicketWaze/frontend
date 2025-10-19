ed735a16f9dc8464d6967eff65d5cf428b584c03
ezenard <ezenardr.dev@gmail.com>
2025-10-19 09:19:27 -0400

    Update: team invitation

--- apps/organizer/app/[locale]/auth/invite/[token]/ClientContent.tsx
+++ apps/organizer/app/[locale]/auth/invite/[token]/ClientContent.tsx
@@ -1,46 +1,60 @@
-'use client'
-import { useRouter } from '@/i18n/navigation'
-import { ButtonPrimary } from '@workspace/ui/components/buttons'
-import LoadingCircleSmall from '@workspace/ui/components/LoadingCircleSmall'
-import { InfoCircle } from 'iconsax-react'
-import { useTranslations } from 'next-intl'
-import { useSearchParams } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
-import { toast } from 'sonner'
+"use client";
+import { useRouter } from "@/i18n/navigation";
+import { ButtonPrimary } from "@workspace/ui/components/buttons";
+import LoadingCircleSmall from "@workspace/ui/components/LoadingCircleSmall";
+import { InfoCircle } from "iconsax-react";
+import { useTranslations } from "next-intl";
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
 
-export default function ClientContent({ token }: { token: string }) {
-    const router = useRouter()
-    const [error, setError] = useState()
-    const t = useTranslations('Auth.register')
-    const searchParams = useSearchParams();
-    const accessToken = searchParams.get("accessToken");
-    useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/invite/accept/${token}`, {
-            method : "GET",
-            headers: {
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${accessToken}`
-            },
-        }).then(res => res.json()).then(res => {
-            
-            if (res.status !== 'success') {
-                toast.error(res.message)
-                setError(res.message)
-            } else {
-                toast.success('Success')
-                setTimeout(() => {
-                    router.push('/analytics')
-                }, 2000);
-            }
-        })
-    }, [])
-    return (
-        <>
-            {error ? <div className='flex flex-col items-center gap-12'>
-                <InfoCircle size="80" color='#737C8A' variant='Bulk' />
-                <p className='text-[1.8rem] text-center leading-8 text-neutral-700'>{t('invalidToken')}</p>
-                <ButtonPrimary onClick={() => router.push('/auth/login')}>{t('login')}</ButtonPrimary>
-            </div> : <LoadingCircleSmall />}
-        </>
-    )
+export default function ClientContent({
+  token,
+  accessToken,
+}: {
+  token: string;
+  accessToken: string;
+}) {
+  const router = useRouter();
+  const [error, setError] = useState();
+  const t = useTranslations("Auth.register");
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/invite/accept/${token}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+
+        if (res.status !== "success") {
+          toast.error(res.message);
+          setError(res.message);
+        } else {
+          toast.success("Success");
+          setTimeout(() => {
+            router.push("/analytics");
+          }, 2000);
+        }
+      });
+  }, []);
+  return (
+    <>
+      {error ? (
+        <div className="flex flex-col items-center gap-12">
+          <InfoCircle size="80" color="#737C8A" variant="Bulk" />
+          <p className="text-[1.8rem] text-center leading-8 text-neutral-700">
+            {t("invalidToken")}
+          </p>
+          <ButtonPrimary onClick={() => router.push("/auth/login")}>
+            {t("login")}
+          </ButtonPrimary>
+        </div>
+      ) : (
+        <LoadingCircleSmall />
+      )}
+    </>
+  );
 }
--- apps/organizer/app/[locale]/auth/invite/[token]/page.tsx
+++ apps/organizer/app/[locale]/auth/invite/[token]/page.tsx
@@ -1,17 +1,19 @@
-import LoadingCircleSmall from '@workspace/ui/components/LoadingCircleSmall'
-import React from 'react'
-import ClientContent from './ClientContent'
+import LoadingCircleSmall from "@workspace/ui/components/LoadingCircleSmall";
+import React from "react";
+import ClientContent from "./ClientContent";
 
-export default async function InvitePage({
-    params,
-}: {
-    params: Promise<{ token: string }>
-}) {
-    const { token } = await params
-    
-    return (
-        <div className='h-full flex items-center justify-center'>
-            <ClientContent token={token}/>
-        </div>
-    )
+interface PageProps {
+  params: Promise<{ token: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function InvitePage({ params, searchParams }: PageProps) {
+  const { token } = await params;
+  const { accessToken } = await searchParams;
+
+  return (
+    <div className="h-full flex items-center justify-center">
+      <ClientContent token={token} accessToken={accessToken as string} />
+    </div>
+  );
 }
